99f1cd98651d9005c3baab5d89394e4b5b9c8039
David Wells <drwells@email.unc.edu>
2018-08-07 12:04:54 -0400

    Add a script that regenerates the build system.

---
+++ autogen.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+## Filename: autogen.sh
+## Created on 07 Aug 2018 by David Wells
+##
+## Copyright (c) 2018, David Wells
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+##    * Redistributions of source code must retain the above copyright notice,
+##      this list of conditions and the following disclaimer.
+##
+##    * Redistributions in binary form must reproduce the above copyright
+##      notice, this list of conditions and the following disclaimer in the
+##      documentation and/or other materials provided with the distribution.
+##
+##    * Neither the name of The University of North Carolina nor the names of
+##      its contributors may be used to endorse or promote products derived from
+##      this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+## ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+## SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+## INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+## POSSIBILITY OF SUCH DAMAGE.
+
+# This script regenerates the build system.
+#
+# TODO: we presently use some Make extensions to run the tests. Autotools
+# complains that this is not POSIX compliant, but using variables in rules is
+# supported on all of the platforms we run on anyway. I cannot figure out how to
+# use autoreconf in such a way that it does not error out due to this, so
+# instead we call the autotools binaries in sequence here manually.
+set -e
+
+aclocal
+
+libtoolize --copy
+
+automake --add-missing -Wno-error
