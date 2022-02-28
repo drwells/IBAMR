2cff77d44a2ddce5f995c9f0bd98e0a4bd8eb36c
colegruninger97 <50035200+colegruninger97@users.noreply.github.com>
2022-02-28 15:46:45 -0500

    Update examples/navier_stokes/ex7/example.cpp
    Co-authored-by: David Wells <drwells@email.unc.edu>

--- examples/navier_stokes/ex7/example.cpp
+++ examples/navier_stokes/ex7/example.cpp
@@ -1,6 +1,15 @@
-// Config files
-#include <ibamr/config.h>
-
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2022 - 2022 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
 // Headers for basic PETSc functions
 #include <petscsys.h>
 
