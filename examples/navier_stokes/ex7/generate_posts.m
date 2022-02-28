82ef12d20338b8d572cf48b93b69414ae8328da6
colegruninger97 <50035200+colegruninger97@users.noreply.github.com>
2022-02-28 15:10:51 -0500

    Update examples/navier_stokes/ex7/generate_posts.m
    Co-authored-by: David Wells <drwells@email.unc.edu>

--- examples/navier_stokes/ex7/generate_posts.m
+++ examples/navier_stokes/ex7/generate_posts.m
@@ -1,4 +1,15 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ---------------------------------------------------------------------
+%%
+%% Copyright (c) 2022 - 2022 by the IBAMR developers
+%% All rights reserved.
+%%
+%% This file is part of IBAMR.
+%%
+%% IBAMR is free software and is distributed under the 3-clause BSD
+%% license. The full text of the license can be found in the file
+%% COPYRIGHT at the top level directory of IBAMR.
+%%
+%% ---------------------------------------------------------------------
 
 % Problem parameters
 L = 0.08;                 % computational domain linear dimension (cm)
